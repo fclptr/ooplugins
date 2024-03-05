9f9ed4e63f8bfd36a6fab02a608e6513a01791f3
fclptr <fclptr@gmail.com>
2024-03-05 19:15:20 +0100

    Kolejny krok

--- sdkjs-plugins/content/costam/plugin.js
+++ sdkjs-plugins/content/costam/plugin.js
@@ -31,9 +31,9 @@ function somethingToDo() {
                 var oWorksheet = Api.GetActiveSheet();
                 //oWorksheet.SetName("sheet 1");
                 var sName = oWorksheet.GetName();
-                oWorksheet.GetRange("A1").SetValue("Worksheet name: ");
+                oWorksheet.GetRange("A1").SetValue("Some name: ");
                 oWorksheet.GetRange("A1").AutoFit(false, true);
-                oWorksheet.GetRange("B1").SetValue(sName);
+                oWorksheet.GetRange("B1").SetValue("<<<"+sName+">>>");
             }, false);
 		};
 
