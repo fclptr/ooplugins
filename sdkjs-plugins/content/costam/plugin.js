ab9a1c5a387fdf4f5a9d569fa017330f5e11c716
fclptr <fclptr@gmail.com>
2024-03-05 11:52:45 +0100

    kolejne zmiany costam

--- sdkjs-plugins/content/costam/plugin.js
+++ sdkjs-plugins/content/costam/plugin.js
@@ -1,8 +1,10 @@
 (function (window, undefined) {
     window.Asc.plugin.init = function () {
 
+        let that=this;
+
         document.getElementById("btn_calculation").onclick = function() {
-            this.callCommand(function() {
+            that.callCommand(function() {
                 var oWorksheet = Api.GetActiveSheet();
                 //oWorksheet.SetName("sheet 1");
                 var sName = oWorksheet.GetName();
@@ -12,6 +14,15 @@
             }, true);
 		};
         
+        document.getElementById("btn_data_range").onclick = function() {    
+            var oWorksheet = Api.GetActiveSheet();
+            //oWorksheet.SetName("sheet 1");
+            var sName = oWorksheet.GetName();
+            oWorksheet.GetRange("A1").SetValue("Worksheet name: ");
+            oWorksheet.GetRange("A1").AutoFit(false, true);
+            oWorksheet.GetRange("B1").SetValue(sName);
+        };
+
     };
     window.Asc.plugin.button = function (id) {
 
